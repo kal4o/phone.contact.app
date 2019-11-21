ba615e8806d1d589632a42e0dad3047a47f9406b
Kalin Iliev <kalinaki78@abv.bg>
2019-11-21 09:57:11 +0200

    Seventh commit - PhoneNumber Service and controller

--- src/main/java/phone/contact/app/controller/PhoneNumberController.java
+++ src/main/java/phone/contact/app/controller/PhoneNumberController.java
@@ -1,11 +1,34 @@
 package phone.contact.app.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import phone.contact.app.model.PhoneNumber;
+import phone.contact.app.service.PhoneNumberService;
 
 @RestController
 @RequestMapping("api/phonenumber")
 public class PhoneNumberController {
 
+    private final PhoneNumberService phoneNumberService;
+
+    @Autowired
+    public PhoneNumberController(PhoneNumberService phoneNumberService) {
+        this.phoneNumberService = phoneNumberService;
+    }
+
+    @PostMapping
+    public void createOrUpdatePhoneNumber(@RequestBody PhoneNumber phoneNumber) {
+        phoneNumberService.createOrUpdatePhoneNumber(phoneNumber);
+    }
+
+    @GetMapping("/{phoneNumberId}")
+    public PhoneNumber getContact(@PathVariable("phoneNumberId") Integer phoneNumberId) {
+        return phoneNumberService.getPhoneNumber(phoneNumberId);
+    }
+
+    @DeleteMapping("/{phoneNumberId}")
+    public void deletePhoneNumber(@PathVariable("phoneNumberId") Integer phoneNumberId) {
+        phoneNumberService.deletePhoneNumber(phoneNumberId);
+    }
 
 }
